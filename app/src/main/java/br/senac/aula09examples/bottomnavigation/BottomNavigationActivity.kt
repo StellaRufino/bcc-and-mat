cbd4616708ea21ab870d435005e291567924f098
Stella Rufino <61765304+StellaRufino@users.noreply.github.com>
2022-04-08 21:59:00 -0300

    Codigo do menu

--- app/src/main/java/br/senac/aula09examples/bottomnavigation/BottomNavigationActivity.kt
+++ app/src/main/java/br/senac/aula09examples/bottomnavigation/BottomNavigationActivity.kt
@@ -2,7 +2,11 @@ package br.senac.aula09examples.bottomnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.aula09examples.R
 import br.senac.aula09examples.databinding.ActivityBottomNavigationBinding
+import br.senac.aula09examples.fragments.AlbumsFragment
+import br.senac.aula09examples.fragments.ArtistsFragment
+import br.senac.aula09examples.fragments.RecentsFragment
 
 class BottomNavigationActivity : AppCompatActivity() {
 
@@ -14,7 +18,25 @@ class BottomNavigationActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.bottomNavigation.setOnItemReselectedListener {
 
+            when(it.itemId) {
+                R.id.artistas -> {
+                    val frag = ArtistsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                R.id.albuns ->{
+                    val frag = AlbumsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                else ->{
+                    val frag = RecentsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+            }
+
+            true
+        }
 
     }
 
